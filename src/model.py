7c9ea7235dc2e492cbaf36dd39f08a1bb532852a
Iva Jurković <ij.poslovni@outlook.com>
2025-01-12 21:17:49 +0100

    [GUI][Model] Updates all around

--- src/model.py
+++ src/model.py
@@ -1,4 +1,7 @@
 import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from sklearn.model_selection import train_test_split
 
 
 files = ['Croatia_Cities.txt', 'Canada_Cities.txt', 'Deutschland_Cities.txt', 'UK_Cities.txt', 'US_Cities.txt','Spain_Cities.txt','France_Cities.txt']
@@ -20,7 +23,6 @@ def get_names(filename, country_tag):
         vectors[char] = [0] * len(chars)
         vectors[char][idx] = 1
 
-    
     return (names, chars, vectors)
 
 
@@ -35,3 +37,22 @@ FRA_names, FRA_chars, FRA_vectors = get_names('France_Cities.txt','FRA')
 
 #char_to_idx = {char: idx for idx, char in enumerate(chars)}
 #idx_to_char = {idx: char for char, idx in char_to_idx.items()}
+
+language_chars = [CRO_chars, CAN_chars, GER_chars, UK_chars, USA_chars, SPN_chars, FRA_chars]
+city_names = [CRO_names, CAN_names, GER_names, UK_names, USA_names, SPN_names, FRA_names]
+
+# ovdje ce ulaz biti X, y, test
+# X nam je sequence - [[1], [1, 2], [1, 2, 3]] (redoslijed slova imena)
+# y nam je [2, 3] - sljedeca slova
+# treba nam padding jer cemo sigurno imati najduze ime koje se isto tako predaje kao input length u LSTMu
+X_train, X_pred, y_train, y_pred = train_test_split(X, y, test_size=0.3)
+
+model = Sequential()
+model.add(Embedding(input_dim=len(language_chars[selected_country]), output_dim="100", input_length=max(len(name) for name in city_names[selected_country])))
+model.add(LSTM(units=128))
+model.add(Dense(units=len(language_chars[selected_country]), activation='softmax'))
+
+model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=['accuracy'])
+
+# batch_size -> nakon koliko se primjera updateaju tezine modela
+fitting = modle.fit(X_train, y_train, validation_data(X_pred, y_pred), epochs=30, batch_size=32)
