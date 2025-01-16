54958f1002bf0966f723833a6792b465efd7fab4
matejLogarusic <matej.logarusic@gmail.com>
2025-01-16 02:17:18 +0100

    Uređen model.py

--- src/model.py
+++ src/model.py
@@ -76,28 +76,6 @@ class NamesDatasetToken:
         # Podjela na trening i testni skup
         return split_data(self.X, self.y, test_size=0.2)
 
-
-    #load_names bez podjele podataka na trening i test
-    # def load_names(self) -> tuple:  
-    #     with open(self.linfo.filename, 'r', encoding='utf-8') as file:
-    #         names = [line.strip() + '<' for line in file.readlines()]
-
-    #     self.tokenizer = Tokenizer(char_level=True)
-    #     self.tokenizer.fit_on_texts(names)
-
-    #     sequences = self.tokenizer.texts_to_sequences(names)
-
-    #     for seq in sequences:
-    #         for i in range(1, len(seq)):
-    #             self.X.append(seq[:i])
-    #             self.y.append(seq[i])
-
-    #     max_seq_length = max([len(seq) for seq in self.X])
-    #     self.X = pad_sequences(self.X, maxlen=max_seq_length, padding='pre')
-
-    #     self.y = tf.keras.utils.to_categorical(self.y, num_classes=len(self.tokenizer.word_index) + 1)
-    #     return (self.X, self.y)
-
 def train_and_save_models(datasets):
     for dataset in datasets:
         lang_code = dataset.linfo.langname
@@ -112,25 +90,7 @@ def train_and_save_models(datasets):
         print(f"Treniram model za jezik: {lang_code}")
 
         # Učitavanje podataka
-        #X, y = dataset.load_names() #bez test podataka
         X_train, X_test, y_train, y_test = dataset.load_names()
-
-        # Kreiranje modela bez unakrsne validacije i test podataka
-        # model = Sequential()
-        # model.add(Embedding(input_dim=len(dataset.tokenizer.word_index)+1, output_dim=100, input_length=X.shape[1]))
-        # model.add(LSTM(units=128))
-        # model.add(Dense(units=len(dataset.tokenizer.word_index)+1, activation='softmax'))
-
-        # model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=['accuracy'])
-
-        # # Treniranje modela
-        # model.fit(X, y, epochs=100, verbose=1)
-
-        # # Spremanje modela i tokenizera
-        # model.save(model_path)
-        # with open(tokenizer_path, 'wb') as f:
-        #     pickle.dump(dataset.tokenizer, f)
-
 
         # Model
         model = Sequential([
@@ -195,56 +155,22 @@ datasets = [
 ]
 
 def load_model_and_tokenizer(lang_code):
-    # Putanje do spremljenih modela i tokenizera
     model_path = f'../saved_models/{lang_code}_model.h5'
     tokenizer_path = f'../saved_models/{lang_code}_tokenizer.pkl'
-    
-    # Učitavanje modela
     model = load_model(model_path)
-    
-    # Učitavanje tokenizera
+
     with open(tokenizer_path, 'rb') as f:
         tokenizer = pickle.load(f)
     
     return model, tokenizer
 
-#trenutno staticki, inace se bira na GUI-u
-# curr_country = 'CRO'
-# curr_dataset = [d for d in datasets if d.linfo.langname == curr_country][0]
-# X, y = curr_dataset.load_names()
-
-# model = Sequential()
-# model.add(Embedding(input_dim=len(curr_dataset.tokenizer.word_index)+1, output_dim=100, input_length=max(len(name) for name in X)+1))
-# model.add(LSTM(units=128))
-# model.add(Dense(units=len(curr_dataset.tokenizer.word_index)+1, activation='softmax'))
-
-# model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=['accuracy'])
-
-# # Compile the model
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# # Define a custom callback to store training progress
-# class TrainingProgressCallback(Callback):
-#     def on_train_begin(self, logs=None):
-#         self.epochs = []
-#         self.accuracies = []
-#         self.losses = []
-        
-#     def on_epoch_end(self, epoch, logs=None):
-#         accuracy_percentage = logs.get('accuracy') * 100
-#         loss_percentage = logs.get('loss') * 100
-#         self.epochs.append(epoch + 1)
-#         self.accuracies.append(accuracy_percentage)
-#         self.losses.append(loss_percentage)
-
 def generate_name(model, tokenizer, max_seq_length, stop_char='<'):
     valid_chars = [char for char, index in tokenizer.word_index.items() if char.isalpha() and char != stop_char]
-    # Nasumično odabiremo početni znak
-    seed_text = random.choice(valid_chars)
+    seed_text = random.choice(valid_chars) # Nasumično odabiremo početni znak
     generated_text = seed_text.upper()  # Inicijalizira generirani tekst s nasumičnim početnim znakom
-    capitalize_next = False  # Zastavica za veliko slovo nakon zareza
+    capitalize_next = False  # Zastavica za veliko slovo nakon razmaka
     ind = 0
-    max_iterations = 100  # Ograničenje iteracija za sprječavanje beskonačne petlje
+    max_iterations = 100 
     
     while ind < max_iterations:
         print(f"Alo {ind}, Seed text: '{seed_text}'")
@@ -270,10 +196,10 @@ def generate_name(model, tokenizer, max_seq_length, stop_char='<'):
         if len(generated_text) == 1:
             generated_text = generated_text.upper() 
         
-        # Ako je prethodni znak bio zarez, naredni znak čini velikim slovom
+        # Ako je prethodni znak bio razmak, naredni znak čini velikim slovom
         if capitalize_next and predicted_char.isalpha():
             predicted_char = predicted_char.upper()
-            capitalize_next = False  # Resetiraj zastavicu
+            capitalize_next = False
 
         # Provjera za razmaka i postavljanje zastavice
         if predicted_char == ' ':
@@ -288,14 +214,6 @@ def generate_name(model, tokenizer, max_seq_length, stop_char='<'):
         print("Prekida se nakon maksimalnog broja iteracija")
 
     return generated_text
-
-# # Train the model with the progress callback
-# progress_callback = TrainingProgressCallback()
-# model.fit(X, y, epochs=100, verbose=2, callbacks=[progress_callback])
-
-# max_seq_length = max([len(seq) for seq in X])  # Maksimalna duljina sekvenci iz trening skupa
-# new_name = generate_name(model, curr_dataset.tokenizer, max_seq_length)
-# print(f"Generirani naziv: {new_name}")
 
 train_and_save_models(datasets)
 
